c3309a6a24354483205d5f94233ac19ef05a4e02
Josevan Oliveira <josivantarcio@gmail.com>
2021-04-27 11:01:11 -0300

    fase #2
    Iniciando a parte 2 do projeto. Será a leitura das possibilidades.

--- resolveSudoku.py
+++ resolveSudoku.py
@@ -3,12 +3,11 @@ bloco = [[],[],[],[],[],[],[],[],[]]
 
 for l in range(9):
     for c in range(9):
-        #while True:
-            n = randint(1, 9) #int(input(f"Digite o valor [{l}][{c}] "))
-            if n >= 0 and n <10:
-         #       print(f'Estamos na {l}{c} e o numero é {n}')
-          #      break
-           # else:
+        while True:
+            n = int(input(f"Digite o valor [{l}][{c}] "))
+            if n <= 0 and n > 10:
+                break
+            else:
                if n >= 0 and n < 10:
                     if l <= 2:
                         if c >= 0 and c <= 2:
@@ -37,22 +36,19 @@ for i in range(len(bloco)):
 
 # ============  Parte 02 =============
 
-celula = []
-#while True:
-#    if 0 not in bloco:
-#        break
-#    else:
-
-for cont in range(9): 
-    for i in range(1,9):
-        if i in bloco[cont]:
-            print(f'{i} faz parte do Bloco {cont}')
-        else:
-            for l in range(9):
-                for c in range(9):
-                    if i not in bloco[c]:
-                        print(f'{i} NãO está na linha')
-                        if i not in bloco[l]:
-                            print(f'{i} não está na coluna')
-                            break
-                        
+while True:
+    if 0 not in bloco:
+        break
+    else:
+        for cont in range(9): 
+            for i in range(1,9):
+                if i in bloco[cont]:
+                    print(f'{i} faz parte do Sub-bloco {cont}')
+                else:
+                    for l in range(9):
+                        for c in range(9):
+                            if i not in bloco[c]:
+                                print(f'{i} NãO está na linha')
+                                if i not in bloco[l]:
+                                    print(f'{i} não está na coluna')
+                                    break
