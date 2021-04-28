fe995470553f089b5a87a11bdda4dc0552cdf7f9
Josevan Oliveira <josivantarcio@gmail.com>
2021-04-28 10:38:12 -0300

    Update resolveSudoku.py
    inicio da Branch da segunda fase

--- resolveSudoku.py
+++ resolveSudoku.py
@@ -1,54 +1,50 @@
 from random import randint
-bloco = [[],[],[],[],[],[],[],[],[]]
+subBloco = [[],[],[],[],[],[],[],[],[]]
 
 for l in range(9):
     for c in range(9):
-        while True:
-            n = int(input(f"Digite o valor [{l}][{c}] "))
-            if n <= 0 and n > 10:
-                break
-            else:
-               if n >= 0 and n < 10:
-                    if l <= 2:
-                        if c >= 0 and c <= 2:
-                            bloco[0].append(n)
-                        if c >= 3 and c <= 5:
-                            bloco[1].append(n)
-                        if c >= 6:
-                            bloco[2].append(n)
-                    if l > 2 and l <= 5:
-                        if c >= 0 and c <= 2:
-                            bloco[3].append(n)
-                        if c >= 3 and c <= 5:
-                            bloco[4].append(n)
-                        if c >= 6:
-                            bloco[5].append(n)
-                    if l > 5 and l <= 8:
-                        if c >= 0 and c <= 2:
-                            bloco[6].append(n)
-                        if c >= 3 and c <= 5:
-                            bloco[7].append(n)
-                        if c >= 6:
-                            bloco[8].append(n)
-for i in range(len(bloco)):
-    print(bloco[i])
+        n = int(input(f"Digite o valor [{l}][{c}] "))
+        if n >= 0 and n < 10:
+            if l <= 2:
+                if c >= 0 and c <= 2:
+                    subBloco[0].append(n)
+                if c >= 3 and c <= 5:
+                    subBloco[1].append(n)
+                if c >= 6:
+                    subBloco[2].append(n)
+            elif l > 2 and l <= 5:
+                if c >= 0 and c <= 2:
+                    subBloco[3].append(n)
+                if c >= 3 and c <= 5:
+                    subBloco[4].append(n)
+                if c >= 6:
+                    subBloco[5].append(n)
+            if l > 5 and l <= 8:
+                if c >= 0 and c <= 2:
+                    subBloco[6].append(n)
+                if c >= 3 and c <= 5:
+                    subBloco[7].append(n)
+                if c >= 6:
+                    subBloco[8].append(n)
+for i in range(len(subBloco)):
+    print(subBloco[i])
 
 
 # ============  Parte 02 =============
-
+celula = []
 while True:
-    if 0 not in bloco:
+    if 0 not in subBloco:
         break
     else:
         for cont in range(9): 
             for i in range(1,9):
-                if i in bloco[cont]:
+                if i in subBloco[cont]:
                     print(f'{i} faz parte do Sub-bloco {cont}')
                 else:
                     for l in range(9):
                         for c in range(9):
-                            if i not in bloco[c]:
+                            if i not in subBloco[c]:
                                 print(f'{i} NãO está na linha')
-                                if i not in bloco[l]:
+                                if i not in subBloco[l]:
                                     print(f'{i} não está na coluna')
                                     break
